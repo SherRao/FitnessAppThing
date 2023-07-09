32be6a01643bb9994d02ca0b79257c16f989f41e
Nausher Rao <sherrao15@gmail.com>
2023-07-09 01:21:39 -0400

    Add basic connection to notion backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,32 @@
 import express, { Express, Request, Response } from "express";
 import { Client as NotionClient } from "@notionhq/client";
+import "dotenv/config";
 
-const hostname = "localhost"; //process.env.HOSTNAME;
-const port = 8080; //process.env.PORT;
-const notionDatabaseId = 0; //process.env.NOTION_DATABASE_ID;
-const notionSecret = "0"; //process.env.NOTION_SECRET;
+const hostname = process.env.HOSTNAME;
+const port = process.env.PORT;
+const notionSecret = process.env.NOTION_SECRET;
+const notionDatabaseId: string = process.env.NOTION_DATABASE_ID ?? "";
 
 const server: Express = express();
 const notion = new NotionClient( {auth: notionSecret} );
 
-server.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+console.log(process.env);
+server.get('/', async (req: Request, res: Response) => {
+  const response = await queryDatabase(notionDatabaseId);
+  res.send(response);
 });
 
 server.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://${hostname}:${port}`);
 });
+
+const queryDatabase = async (databaseId: string) => {
+    try {
+        const response = await notion.databases.query({
+            database_id: databaseId,
+          });
+        return response.results[0];
+    } catch (error: any){
+        console.log(error.body);
+    }
+}
